4cb6f28a533456264f4f596c695499c70c278e62
Andy Cobley <acobley@computing.dundee.ac.uk>
2017-06-05 13:32:55 +0100

    Initial Commit with new code

--- puremodular/pureADSR/GMSN_ADSR_Code_20161023/GMSN_ADSR_Code_20161023.ino
+++ puremodular/pureADSR/GMSN_ADSR_Code_20161023/GMSN_ADSR_Code_20161023.ino
@@ -1,9 +1,45 @@
+/*
+   SA - Share & Adapt.
+   BY - Credit where credit is due.
+   For any purpose including Commercial and Group Buys.
+   No pressure to share design files.
+   R&D funded by donation.
+   https://gmsn.co.uk/products/r-d-funded-by-donations
+
+   Creative Commons License
+   Licensed under a Creative Commons Attribution 4.0 International License.
+   http://creativecommons.org/licenses/by/4.0/
+
+   Source from
+   https://gmsn.co.uk/products/gmsn-pure-adsr
+
+   original design and code from:
+   https://gmsn.co.uk/
+
+   This version by A.Cobley
+   andy@r2-dvd.org
+*/
+
+
+
+
 #include "SPI.h"
 
 float aPot, aCoeff, enVal = 0, dPot, dCoeff, sPot, sCoeff, sVal, rPot, rCoeff;
 boolean gate = 0, rising = 0;
 int buttonState, lastButtonState = HIGH, loopStage = 0, x = 0;
 long lastDebounceTime = 0, debounceDelay = 500;
+int TRIGGER = 2;
+int BUTTONTRIGGER = 3;
+int BUTTON = 4;
+int GATEIN = 5;
+int SW1 = 6;
+int SW2 = 7;
+int DACCS = 10;
+
+float alpha=0.25;
+float alphaPower=(alpha-1)/alpha;
+float dx=1/1024;
 
 void setup() {
   //DAC Comms
@@ -11,200 +47,90 @@ void setup() {
   SPI.setBitOrder(MSBFIRST);
 
   //Pins
-  pinMode(10, OUTPUT); //DAC CS
-  pinMode(2, INPUT); //TRIGGER IN
-  pinMode(3, INPUT); //BUTTON TRIGGER
-  pinMode(4, INPUT); //Button
-  pinMode(5, INPUT); //Gate In
-  pinMode(6, INPUT); //MODE SW1
-  pinMode(7, INPUT); //MODE SW2
-  digitalWrite(10, HIGH);
+  pinMode(DACCS, OUTPUT); //DAC CS
+  pinMode(TRIGGER, INPUT); //TRIGGER IN  NOTE, this is inverted
+  pinMode(BUTTONTRIGGER, INPUT); //BUTTON TRIGGER
+  pinMode(BUTTON, INPUT); //Button
+  pinMode(GATEIN, INPUT); //Gate In
+  pinMode(SW1, INPUT); //MODE SW1
+  pinMode(SW2, INPUT); //MODE SW2
+  digitalWrite(DACCS, HIGH);
 
   //Interupts
-  attachInterrupt(digitalPinToInterrupt(2), gateOn, FALLING);
+  attachInterrupt(digitalPinToInterrupt(TRIGGER), gateOn, FALLING); //Actually on rising, the gate is inverted.
+  //  Say we are alive by flashing the LED
+  flash (10, 500);
+}
 
+void GetPots(){
+  aPot = map(analogRead(A3), 0, 1024, 1024, 0);
+  dx=aPot/1024;
+  aCoeff = alpha*pow(enVal/4100,alphaPower)*dx;
+  dPot = map(analogRead(A2), 0, 1024, 200, 0);
+  sPot = map(analogRead(A1), 0, 1024, 0, 4096);
+  rPot = map(analogRead(A0), 0, 1024, 80, 0);
 }
 
 void loop() {
+  GetPots();
+  
+  if (rising) {
 
-  //Fast attack label
-attack:
-
-  //Check if not in Trap Mode
-  if (digitalRead(6) != HIGH) {
-
-    //Check if Gate is On
-    if (digitalRead(4) == LOW || digitalRead(5) == LOW) {
-
-      //Attack
-      if (rising) {
-        mcpWrite((int)enVal);
-        //delay(5);
-        if (enVal == 4095) {
-          rising = 0;
-        }
-      }
-
-      //Not Attack
-      if (!rising) {
-
-        //Check if in AR Mode and goto release
-        if (digitalRead(6) == LOW && digitalRead(7) == LOW) {
-          goto rlease;
-        }
-
-        //else continue with decay to sustain
-        dPot = map(analogRead(A2), 0, 1024, 200, 0);
-        float dCoeff = dPot / 16384;
-        sPot = map(analogRead(A1), 0, 1024, 0, 4096);
-        enVal += dCoeff * (sPot - enVal);
-        mcpWrite((int)enVal);
-      }
+    //Removed fast attack
+    //float aCoeff = aPot / 16384;
+    //enVal += aCoeff * (4311 - enVal);
+    enVal+=aCoeff * 4100;
+    if (enVal > 4095) {
+      enVal = 4095;
     }
+  }
 
-    // If no Gate, write release values
-    else {
+  //Check if Gate is On
+  if (digitalRead(GATEIN) == LOW) { // Inverted
 
-      //Quick release label
-rlease:
-
-      rPot = map(analogRead(A0), 0, 1024, 80, 0);
-      float rCoeff = rPot / 16384;
-      enVal += rCoeff * (-100 - enVal);
-      if (enVal < 0) {
-        enVal = 0;
+    //Attack
+    if (rising) {
+      mcpWrite((int)enVal);
+      //delay(5);
+      if (enVal == 4095) {
+        rising = 0;
       }
+    } else {
+
+      
+
+      //else continue with decay to sustain
+
+      float dCoeff = dPot / 16384;
+
+      enVal += dCoeff * (sPot - enVal);
       mcpWrite((int)enVal);
     }
-
-    //Poll Trigger Button, debounce, initialise and goto Attack
-    int reading = digitalRead(4);
-
-    if ((millis() - lastDebounceTime) > debounceDelay) {
-
-      if (reading != buttonState) {
-        buttonState = reading;
-        if (buttonState == LOW) {
-          enVal = 0;
-          rising = 1;
-          rising = 1;
-          lastDebounceTime = millis();
-          goto attack;
-        }
-      }
-    }
-    lastButtonState = reading;
-
-    //Get Attack Values. Moved down here to enable fast attack
-    if (rising) {
-      aPot = map(analogRead(A3), 0, 1024, 1024, 0);
-      if (aPot == 1024) {
-        enVal = 4095;
-      } else {
-        float aCoeff = aPot / 16384;
-        enVal += aCoeff * (4311 - enVal);
-        if (enVal > 4095) {
-          enVal = 4095;
-        }
-      }
-    }
-    delayMicroseconds(50);
-
-  } else {
-    //Trap Mode
-    switch (loopStage) {
-
-      //if Up
-      case 0:
-        {
-          //if enVal < 4096
-          //write enVal
-          aPot = map(analogRead(A3), 0, 1024, 200, 0);
-          float aCoeff = aPot / 16384;
-          enVal += aCoeff * (4311 - enVal);
-          if (enVal > 4096) {
-            enVal = 4095;
-            mcpWrite((int)enVal);
-            loopStage = 1;
-            break;
-          }
-          mcpWrite((int)enVal);
-        }
-        break;
-
-      //If Hold
-      case 1:
-        {
-          //if x < dPot
-          dPot = map(analogRead(A2), 0, 1024, 0, 30000);
-          mcpWrite((int)enVal);
-          x++;
-          if (x > dPot) {
-            mcpWrite((int)enVal);
-            x = 0;
-            loopStage = 2;
-            break;
-          }
-        }
-        break;
-
-      //if Down
-      case 2:
-
-        {
-          //if enVal > 0
-          //write enVal
-          sPot = map(analogRead(A1), 0, 1024, 200, 0);
-          float sCoeff = sPot / 16384;
-          enVal += sCoeff * (-100 - enVal);
-          if (enVal < 0) {
-            enVal = 0;
-            mcpWrite((int)enVal);
-            loopStage = 3;
-            break;
-          }
-          mcpWrite((int)enVal);
-        }
-        break;
-
-      //if Off
-      case 3:
-        {
-          rPot = map(analogRead(A0), 0, 1024, 0, 32800);
-          if (x < rPot) {
-            //write hold value
-            mcpWrite((int)enVal);
-            x ++;
-          } else {
-            //else Turn on Down
-            x = 0;
-            loopStage = 0;
-            break;
-          }
-        }
-        break;
-
-    }
-
-    //Poll Trigger Button, debounce, initialise Up phase
-    int reading = digitalRead(4);
-
-    if ((millis() - lastDebounceTime) > debounceDelay) {
-
-      if (reading != buttonState) {
-        buttonState = reading;
-        if (buttonState == LOW) {
-          enVal = 0;
-          x = 0;
-          loopStage = 0;
-          lastDebounceTime = millis();
-        }
-      }
-    }
-    lastButtonState = reading;
-    delayMicroseconds(50);
   }
+
+  // If no Gate, write release values
+  else {
+
+    //Quick release label
+rlease:
+
+
+    float rCoeff = rPot / 16384;
+    enVal += rCoeff * (-100 - enVal);
+    if (enVal < 0) {
+      enVal = 0;
+    }
+    mcpWrite((int)enVal);
+  }
+
+
+
+  delayMicroseconds(50);
+
+
+  delayMicroseconds(50);
 }
+
 
 //Interrupt routine for rising edge of Gate
 void gateOn() {
@@ -219,7 +145,7 @@ void gateOn() {
 void mcpWrite(int value) {
 
   //CS
-  digitalWrite(10, LOW);
+  digitalWrite(DACCS, LOW);
 
   //DAC1 write
 
@@ -232,8 +158,8 @@ void mcpWrite(int value) {
   data = value;
   SPI.transfer(data);
 
-  // Set digital pin 10 HIGH
-  digitalWrite(10, HIGH);
+  // Set digital pin DACCS HIGH
+  digitalWrite(DACCS, HIGH);
 }
 
 //Test function for flashing the led. Value = no of flashes, time = time between flashes in mS
